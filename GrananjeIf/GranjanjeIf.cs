f37d19f70557459003a229302e7247c56af4bcf0
Lovro Vrkic <lovrovr@hotmail.com>
2015-11-05 20:31:34 +0100

    usporedbeRealnihBrojeva solved

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -13,7 +13,7 @@ namespace Vsite.CSharp
             {
                 int broj = int.Parse(unos);
 
-                // TODO: Napisati grananja if koja će za uneseni broj:
+                //Napisati grananja if koja će za uneseni broj:
                 // 1. provjeriti je li broj paran. Ako je broj paran, ispisat će poruku "broj N je djeljiv s 2", a inače će
                 // 2. provjeriti je li broj djeljiv s 3. Ako je broj djeljiv, ispisat će poruku "broj N je djeljiv s 3", a inače će
                 // 3. provjeriti je li broj djeljiv s 5. Ako je broj djeljiv, ispisat će poruku "broj N je djeljiv s 5", a inače će
